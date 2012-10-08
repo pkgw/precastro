57a3f2c6bc69f852a064622616a40fefdce3f7bd
Peter Williams <peter@newton.cx>
2012-10-08 14:01:24 -0400

    precastro.py: document the Time class

--- precastro.py
+++ precastro.py
@@ -64,41 +64,116 @@ def _checksofacode (func, code, dubiousok):
 
 
 class Time (object):
+    """A precisely-measured time and its associated timescale.
+
+Times are stored in the IAU SOFA format: as two double-precision
+floating point numbers specifying a Julian Date. Each time is also
+associated with a named timescale, for which 'UTC' and 'TT' are
+currently well-supported.
+
+UTC does not have well-defined behavior regarding leapseconds. SOFA's
+implementation is such that UTC times on days with leapseconds will
+progress at a different rate than on other days. Any kind of precise
+measurement, and any math with :class:`Time` objects, should avoid UTC
+at all costs. The only way to safely interchange UTC times is in
+broken-down calendar format, so that leapseconds can be expressed as
+(e.g.) HH:MM:60.375.
+"""
+
     jd1 = None
+    """The first Julian Date component."""
+
     jd2 = None
+    """The second Julian Date component."""
+
     timescale = None
+    """The timescale in which this time is measured. 'UTC' and 'TT'
+    are somewhat implemented; additional support for TAI, UT1, TCG,
+    TCB, and TDB is possible but not yet exposed in the Python."""
 
     def fromNow (self):
-        # There seems to be no way to get the current time
-        # that avoids the leapsecond ambiguity ... ?!?
+        """Set the object to represent the current time according
+to the computer clock. The associated timescale is UTC.
+
+:returns: *self*
+
+There (bizarrely) seems to be no way to get the current time that
+avoids the leapsecond ambiguity inherent in the POSIX time system,
+so this value has ~1-second precision on leapsecond days. Things
+should be better on non-leapsecond days, though of course the
+accuracy of the system clock is a limitation.
+
+As of summer 2012 there was apparently interest in the Linux community
+in adding a CLOCK_TAI clockid_t for ``clock_gettime`` that would make
+this all so much easier. It is possible that using the tzdata
+"right/UTC" timezone would get things right, but that just seems
+impossible to test, and not worth doing unless I can be sure that it's
+actually right. Another source references ``ntp_adjtime(3)`` as a
+possibility for querying the leapsecond subsystem to get this right.
+"""
         from time import time
         self.fromPOSIX (time ())
         return self
 
 
     def fromPOSIX (self, time):
-        # "TAI-10" standard is appealing but not honored, so there's a
-        # leapsecond ambiguity and we just can't do anything about it.
-        # Based on SOFA's convention for representing UTC as a JD, our
-        # approach here will result in a constant slew across each day
-        # that contains a leapsecond, while in theory we could get
-        # full precision except during the leapseconds themselves,
-        # when there'd be a +/- one-second ambiguity.
+        """Set the object to represent the given POSIX time. The
+associated timescale is UTC.
+
+:arg time: a POSIX/Unix time
+:type time: convertable to :class:`float`
+:returns: *self*
+
+It really seems as if we just can't do anything about the leapsecond
+ambiguity: the "TAI minus 10" standard is appealing but apparently not
+honored. We only really need this function to implement fromNow(), so
+if I ever find a way to get the current time in a way that's
+leapsecond-safe, then this becomes a lot less pressing.
+
+Note while both UTC and POSIX time are problematic regarding
+leapseconds, they are problematic in different and non-canceling ways.
+"""
         self.jd1 = 2440587.5
-        self.jd2 = time / 86400.
+        self.jd2 = float (time / 86400.)
         self.timescale = 'UTC'
         return self
 
 
     def fromJD (self, jd, timescale):
+        """Set the object to represent the given Julian Date and
+timescale.
+
+:arg jd: a Julian date
+:type jd: convertable to :class:`float`
+:arg timescale: the timescale being used (see :class:`Time` docs)
+:type timescale: :class:`str`
+:returns: *self*
+
+Storing a JD as a single float limits precision to about 20.1
+microseconds, according to Kaplan, Bartlett, & Harris (2011, USNO-AA
+Tech Note 2011-02).
+"""
         _checktimescale (timescale)
-        self.jd1 = jd
+        self.jd1 = float (jd)
         self.jd2 = 0.0
         self.timescale = timescale
         return self
 
 
     def fromMJD (self, mjd, timescale):
+        """Set the object to represent the given modified Julian Date and
+timescale.
+
+:arg mjd: a modified Julian date
+:type mjd: convertable to :class:`float`
+:arg timescale: the timescale being used (see :class:`Time` docs)
+:type timescale: :class:`str`
+:returns: *self*
+
+JD = MJD + 2400000.5. This will offer some precision gains over a
+double-precision representation of an full, un-modified Julian Date, but
+they're probably not too large. Maybe mentioned in Kaplan et al (2011)?
+"""
         _checktimescale (timescale)
         self.jd1 = 2400000.5
         self.jd2 = mjd
@@ -108,6 +183,38 @@ class Time (object):
 
     def fromCalendar (self, year, month, day, hour, minute, second, timescale,
                       dubiousok=False):
+        """Set the object to represent the given Gregorian calendar
+date in the given timescale.
+
+:arg year: the year
+:type year: convertable to :class:`int`
+:arg month: the month
+:type month: convertable to :class:`int`, range 1 to 12
+:arg day: the day
+:type day: convertable to :class:`int`, range 1 to 31
+:arg hour: the hour
+:type hour: convertable to :class:`int`, range 0 to 23
+:arg minute: the minute
+:type minute: convertable to :class:`int`, range 0 to 59
+:arg second: the second
+:type second: convertable to :class:`float`, range 0 to 60.99999...
+:arg timescale: the timescale being used (see :class:`Time` docs)
+:type timescale: :class:`str`
+:arg dubiousok: whether to accept extreme years
+:type dubiousok: :class:`bool`
+:returns: *self*
+:raises: :exc:`SofaError` for bad inputs
+
+Uses the Gregorian calendar system, which has the 100- and 400-year
+leap day rules. SOFA docs say that the algorithm is valid to -4799
+January 1 and uses the "proleptic Gregorian calendar" without paying
+attention to, say, when the Gregorian calendar was actually adopted.
+
+This is currently the only way to set a UTC date that can properly
+account for leapseconds. But I know of no way to get the current
+computer time in this broken-down format without going through POSIX
+time, so that's not actually helpful.
+"""
         _checktimescale (timescale)
         code, self.jd1, self.jd2 = _precastro.iauDtf2d (timescale, year, month, day,
                                                         hour, minute, second)
@@ -117,14 +224,45 @@ class Time (object):
 
 
     def asJD (self):
+        """Return the time as a Julian Date.
+
+:returns: the time as a Julian Date
+:rtype: :class:`float`
+
+The one-value representation limits precision to about 20
+microseconds.
+"""
         return self.jd1 + self.jd2
 
 
     def asMJD (self):
+        """Return the time as a modified Julian Date.
+
+:returns: the time as a modified Julian Date
+:rtype: :class:`float`
+
+The precision may be somewhat superior to that given by :meth:`asJD`,
+but the difference will probably be minor.
+"""
         return (self.jd1 - 2400000.5) + self.jd2
 
 
     def asTT (self, dubiousok=False):
+        """Return a :class:`Time` object equivalent to this one, but
+in the TT timescale.
+
+:arg dubiousok: whether to accept extreme dates
+:type dubiousok: :class:`bool`
+:returns: an equivalent time in the TT timescale
+:rtype: :class:`Time`
+:raises: :exc:`SofaError` if extrapolating to an unacceptably extreme date
+:raises: :exc:`UnsupportedTimescaleError` if there's no implementation
+  to convert *self* to TT.
+
+If the timescale of *self* is a TT, a copy is returned. Right now the
+only other supported timescale is UTC. Other conversions can be
+implemented as the need arises.
+"""
         res = Time ()
         res.timescale = 'TT'
 
@@ -142,6 +280,23 @@ class Time (object):
 
 
     def fmtCalendar (self, precision=0, dubiousok=False):
+        """Format the time as a calendar date/time.
+
+:arg precision: how many decimal places of precision in the second
+  component to use (default: 0, range: > -4)
+:type precision: :class:`int`
+:arg dubiousok: whether to accept extreme dates
+:type dubiousok: :class:`bool`
+:returns: the time formatted as a calendar date/time
+:rtype: :class:`str`
+:raises: :exc:`SofaError` if using an unacceptably extreme date
+
+The returned string takes the form 'YYYY/MM/DD HH:MM:SS.SSSS', where
+the number of decimal places used for the seconds varies. If *precision*
+is less than one, the decimal point and fractional parts are omitted.
+If precision is less than zero, the reported values are rounded as
+appropriate but trailing zeros are still present in the string.
+"""
         info = _precastro.iauD2dtf_tweak (self.timescale, precision, self.jd1, self.jd2)
         _checksofacode ('d2dtf', info[0], dubiousok)
         ymdhmsf = list (info[1:])
@@ -155,6 +310,13 @@ class Time (object):
 
 
 def now ():
+    """Get the current time.
+
+:returns: the current time from the system clock
+:rtype: :class:`Time`
+
+Shorthand for ``Time().fromNow()``.
+"""
     return Time ().fromNow ()
 
 
