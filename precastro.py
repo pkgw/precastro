93d9f399da8bff66e80f39c48cf07059df1f576b
Peter Williams <peter@newton.cx>
2012-10-30 11:58:10 -0400

    precastro.py: semantics fix to Time.fromjepoch()
    I didn't know that Julian epochs are always in the TT timescale. I've
    fixed the function to not take the timescale as a parameter.

--- precastro.py
+++ precastro.py
@@ -223,22 +223,18 @@ time, so that's not actually helpful.
         return self
 
 
-    def fromjepoch (self, julian_epoch, timescale):
-        """Set the object to represent the given Julian epoch and
-timescale.
+    def fromjepoch (self, julian_epoch):
+        """Set the object to represent the given Julian epoch.
 
 :arg julian_epoch: a Julian epoch (eg 2005.37)
 :type julian_epoch: convertable to :class:`float`
-:arg timescale: the timescale being used (see :class:`Time` docs)
-:type timescale: :class:`str`
 :returns: *self*
 
-In the Julian system, a year is exactly 365.25 days long, so the
-conversion to a Julian Date is relatively straightforward.
+A Julian epoch measures years of exactly 365.25 days long in the TT
+timescale.
 """
-        _checktimescale (timescale)
         self.jd1, self.jd2 = _precastro.iauEpj2jd (julian_epoch)
-        self.timescale = timescale
+        self.timescale = 'TT'
         return self
 
 
